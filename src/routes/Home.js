805fdfeeb28b18bcb9b8cdf7d0446e0f316a8cae
Jinhee Kim <jinhee.kim@solarconnect.kr>
2020-10-16 19:06:19 +0900

    영화 목록 요청하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,29 @@
-function Home() {
-  return 'Home';
-}
+import React from 'react'
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-export default Home;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+
+  if (loading) {
+    return 'loading...';
+  }
+  if (data && data.movies) {
+    console.log(data)
+    return data.movies.map((movie) => {
+      return <h1>{movie.title}</h1>
+    });
+  }
+
+};
